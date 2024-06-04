e7b4f8d97804ef36bdfe58a42c8f6a6a82da229c
박정민 <163364733+nicejmp1@users.noreply.github.com>
2024-06-04 09:44:42 +0900

    123

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import Main from '../components/section/Main'
 
 
 const Home = () => {
+
     return (
         <Main
             title="유튜브 플레이리스트"
             description="유튜브 플레이리스트를 모아둔 채널입니다."
         >
-            홈화면 올리기
+
         </Main>
     )
 }
